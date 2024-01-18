11eca755b9bebdb31db0bc1d244d05c3a860cb00
bumjun2 <jbj3713@naver.com>
2024-01-18 09:47:02 +0900

    리스트 조회 및 코드 리펙토링

--- src/main/java/com/study/todoapi/todo/controller/TodoController.java
+++ src/main/java/com/study/todoapi/todo/controller/TodoController.java
@@ -1,11 +1,18 @@
 package com.study.todoapi.todo.controller;
 
+import com.study.todoapi.todo.dto.request.TodoCreateRequestDTO;
+import com.study.todoapi.todo.dto.response.TodoDetailResponseDTO;
+import com.study.todoapi.todo.dto.response.TodoListResponseDTO;
 import com.study.todoapi.todo.entity.Todo;
 import com.study.todoapi.todo.service.TodoService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @Slf4j
@@ -16,12 +23,47 @@ public class TodoController {
 
     // 할 일 등록 요청
     @PostMapping
-    public ResponseEntity<?> createTodo(@RequestBody Todo todo){
-        todoService.create(todo);
+    public ResponseEntity<?> createTodo(
+            @Validated
+            @RequestBody
+            TodoCreateRequestDTO dto
+            , BindingResult result
+    ){
+
+        if (result.hasErrors()){
+            log.warn("dto 검증 에러 : {}", result.getFieldError());
+            return ResponseEntity
+                    .badRequest()
+                    .body(result.getFieldError());
+        }
+
+        try{
+            TodoListResponseDTO dtoList = todoService.create(dto);
+            return ResponseEntity
+                    .ok()
+                    .body(
+                            dtoList
+                    );
+        } catch (Exception e){
+            log.error(e.getMessage());
+            return ResponseEntity.internalServerError().body(
+                    TodoListResponseDTO.builder().error(e.getMessage()).build()
+            );
+        }
+    }
+
+
+    // 할 일 목록 조회 요청
+    @GetMapping
+    public ResponseEntity<?> retrieveTodoList(){
+        log.info("/api/todos GET!");
+
+        TodoListResponseDTO retrieve = todoService.retrieve();
+
         return ResponseEntity
                 .ok()
                 .body(
-                        "ok"
+                    retrieve
                 );
     }
 }
--- src/main/java/com/study/todoapi/todo/service/TodoService.java
+++ src/main/java/com/study/todoapi/todo/service/TodoService.java
@@ -1,11 +1,17 @@
 package com.study.todoapi.todo.service;
 
+import com.study.todoapi.todo.dto.request.TodoCreateRequestDTO;
+import com.study.todoapi.todo.dto.response.TodoDetailResponseDTO;
+import com.study.todoapi.todo.dto.response.TodoListResponseDTO;
 import com.study.todoapi.todo.entity.Todo;
 import com.study.todoapi.todo.repository.TodoRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Slf4j
@@ -15,8 +21,24 @@ public class TodoService {
     private final TodoRepository todoRepository;
 
     //할 일 등록
-    public void create(Todo todo){
-        todoRepository.save(todo);
-        log.info("새로운 할 일이 저장되었습니다. 제목 : {}", todo.getTitle());
+    public TodoListResponseDTO create(TodoCreateRequestDTO dto){
+        todoRepository.save(dto.toEntity());
+        log.info("새로운 할 일이 저장되었습니다. 제목 : {}", dto.getTitle());
+
+        return retrieve();
+    }
+
+    //할 일 몰록 불러오기
+    public TodoListResponseDTO retrieve() {
+
+        List<Todo> todoList = todoRepository.findAll();
+
+        List<TodoDetailResponseDTO> dtoList = todoList.stream()
+                .map(todo -> new TodoDetailResponseDTO(todo))
+                .collect(Collectors.toList());
+
+        return TodoListResponseDTO.builder()
+                .todos(dtoList)
+                .build();
     }
 }
